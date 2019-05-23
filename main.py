9a7c264c3e82ed393d297741d91b5245d01cfb80
Kleebaue <33054921+Kleebaue@users.noreply.github.com>
2019-05-23 11:24:56 +0200

    first version

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 # Exercise 3
 from pathlib import Path
-# import traceback
-# import utils
+import pandas as pd
 
 # import functions from utils here
-
-
+from utils import count
+    
 # 1. Contstruct the path to the text file in the data directory using the `pathlib` module [2P]
 
-data_dir = "/home/mklee/git/exercise-3-Kleebaue/data/"
-output_dir = data_dir + "../solution/"
-
-cars = data_dir + "cars.txt"
+d = Path('/home/mklee/git/exercise-3-Kleebaue')
+data_dir = d / 'data'
+output_dir = d / "solution"
 
 # 2. Read the text file [2P]
-with open(cars, "r") as file:
-    lines = file.readlines()
-        
-print(lines)
+
+with open(data_dir / 'cars.txt', "r") as file:
+    car = file.readlines()
+
+print(car)
 
 # 3. Count the occurences of each item in the text file [2P]
-# count = utils.count_type(lines)
+# import counter from utils to solve task
+
+count(car)
+
+# ------------
+# solving the problem
+from collections import Counter
+Counter(car)
 
 # 4. Using `pathlib` check if a directory with name `solution` exists and if not create it [2P]
 Path('/home/mklee/git/exercise-3-Kleebaue/solution').mkdir(parents=True, exist_ok=True) 
@@ -30,13 +36,3 @@ Path('/home/mklee/git/exercise-3-Kleebaue/solution').mkdir(parents=True, exist_o
 #    item_name_1, item_count_1
 #    item_name_2, item_count_2
 #    ...
-
-single = list(set(lines))
-
-#with open(data_dir + "/solutions/count.csv", "w") as file:
-#    file.write('Type;count/n' )
-#    if len(count) == len(single):
-#        for i in range(len(count)):
-#            #file.write(str(res[i]) + ";" + single[i])
-#            file.writelines(single[i][:-2] + ";" + str(count[i]) +"\n")
-
--- utils.py
+++ utils.py
@@ -2,3 +2,10 @@
 
 # Use this file for all functions you create and might want to reuse later.
 # Import them in the `main.py` script
+
+# ---------
+
+def count(listtocount):
+    [[y,listtocount.count(y)] for y in set(listtocount)]
+    print(dict((y,listtocount.count(y)) for y in set(listtocount)))
+    
